e670827cc8221b0f031abfe596367e6583791b83
ldmora13 <moraparadaluisdavid@gmail.com>
2026-09-02 16:09:00 -0500

    feat: Remove unused asset files

--- server/scripts/copy-template.cjs
+++ server/scripts/copy-template.cjs
@@ -4,7 +4,6 @@ const path = require('node:path');
 const destinationDirectory = path.resolve(__dirname, '../dist/template');
 const assetsDestinationDirectory = path.resolve(__dirname, '../dist/assets');
 const templateNames = ['template_r.pdf', 'template_v.pdf'];
-const assetNames = ['index.png', 'middle.png', 'pinky.png', 'ring.png', 'thumb.png', 'Contact_info_USCIS.png', 'USCIS_Signature_Preferred_FC.png'];
 
 Promise.all([
     fs.mkdir(destinationDirectory, { recursive: true }),
@@ -14,11 +13,7 @@ Promise.all([
         ...templateNames.map((templateName) => fs.copyFile(
             path.resolve(__dirname, '../src/template', templateName),
             path.join(destinationDirectory, templateName),
-        )),
-        ...assetNames.map((assetName) => fs.copyFile(
-            path.resolve(__dirname, '../src/assets', assetName),
-            path.join(assetsDestinationDirectory, assetName),
-        )),
+        ))
     ]))
     .catch((error) => {
         console.error(`Failed to copy PDF template: ${error.message}`);
